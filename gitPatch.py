0c381f08f361d232c752e74fab9c05ffc2cdec43
LAPTOP-E3NS1O95\\User <madgalene@insidersecurity.co>
2021-09-07 19:24:59 +0800

    update

--- gitPatch.py
+++
@@ -1,9 +0,0 @@
-import git
-import os
-
-# git clone repository
-#git.Git("C:\\Users\\User\\git_projects").clone("https://github.com/921am/ccTruthBasket.git")
-
-# git patch
-# os.system("git checkout main")
-os.system("git format-patch main")
