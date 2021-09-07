6b93b5c545ee90a2b977e91f7f68b48fef256856
LAPTOP-E3NS1O95\User <madgalene@insidersecurity.co>
2021-09-07 18:20:52 +0800

    test files for InsiderSecurity

---
+++ gitPatch.py
@@ -0,0 +1,9 @@
+import git
+import os
+
+# git clone repository
+#git.Git("C:\\Users\\User\\git_projects").clone("https://github.com/921am/ccTruthBasket.git")
+
+# git patch
+# os.system("git checkout main")
+os.system("git format-patch main")
