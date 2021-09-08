cd526f6a5cb52d71ecd8de9882ac5bda07350db6
LAPTOP-E3NS1O95\User <madgalene@insidersecurity.co>
2021-09-08 09:52:23 +0800

    update

--- gitPatch_create.py
+++ gitPatch_create.py
@@ -7,20 +7,12 @@ import os.path
 #git.Git("C:\\Users\\User\\git_projects").clone("https://github.com/921am/ccTruthBasket.git")
 
 # C:\\Users\\User\\git_projects\\ccTruthBasket
-# path = input("Enter repo path: ")
-# branch = input("Enter commit branch: ")
-repo = Repo('C:\\Users\\User\\git_projects\\ccTruthBasket')
-repo.git.add(update=True)
-repo.index.commit("update")
-origin = repo.remote(name='origin')
-origin.push()
-
-repo.git.execute(['git','apply','patch.diff'])
+path = input("Enter repo path: ")
+name = input("Enter file name: ")
 
 # create patch files C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test4.patch
-# patchPath = input("Enter path for PATCH file: ")
-# os.system("git status")
-# os.system("git format-patch main --stdout > " + patchPath)
+os.system("git status")
+os.system("git format-patch main --stdout > " + str(name))
 
 # os.system("git apply --stat " + patchPath)
 # os.system("git apply --check " + patchPath)
