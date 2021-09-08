ceadf2f21bbd459f725b113ff6e3f2dcc11fe289
LAPTOP-E3NS1O95\\User <madgalene@insidersecurity.co>
2021-09-08 13:51:01 +0800

    update for patch

--- gitPatch_apply.py
+++ gitPatch_apply.py
@@ -1,12 +1,22 @@
-import git
-from git import repo
 import os
 import os.path
+import sys
 
 # git clone repository
 #git.Git("C:\\Users\\User\\git_projects").clone("https://github.com/921am/ccTruthBasket.git")
 
-# create patch files
-os.system("git apply --stat C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test4.patch")
-os.system("git apply --check C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test4.patch")
-os.system("git am --signoff < C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test4.patch")
+patchPath = input("Enter PATCH file path: ")
+
+# apply patch files
+os.system("git add .")
+os.system("git apply --stat " + str(patchPath))
+os.system("git apply --check " + str(patchPath))
+os.system("git am --signoff < " + str(patchPath))
+os.system("git am --continue")
+
+# if (str(sys.platform) == "win32"):
+#     print(sys.platform)
+    
+# else:
+#     patchPath = input("Enter PATCH file path: ")
+
--- gitPatch_create.py
+++ gitPatch_create.py
@@ -6,14 +6,20 @@ import os.path
 # git clone repository
 #git.Git("C:\\Users\\User\\git_projects").clone("https://github.com/921am/ccTruthBasket.git")
 
+# git commit before creating patch file
+# C:\\Users\\User\\git_projects\\ccTruthBasket
+commitPath = input("Enter repo to commit: ")
+repo = Repo(str(commitPath))
+repo.git.add(update=True)
+repo.index.commit("update for patch")
+origin = repo.remote(name='origin')
+origin.push()
+
+# create patch file
 # C:\\Users\\User\\git_projects\\ccTruthBasket
 path = input("Enter repo path: ")
-name = input("Enter file name: ")
+fileName = input("Enter PATCH file name: ")
 
-# create patch files C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test4.patch
+# create patch files C:\\Users\\User\\git_projects\\ccTruthBasket\\patch_files\\test.patch
 os.system("git status")
-os.system("git format-patch main --stdout > " + str(name))
-
-# os.system("git apply --stat " + patchPath)
-# os.system("git apply --check " + patchPath)
-# os.system("git am --signoff < " + patchPath)
+os.system("git format-patch main --stdout > " + str(path) + str(fileName))
